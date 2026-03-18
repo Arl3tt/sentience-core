b2d4ebf12c40814eae38116b6586a96301e45998
Vanessa Diaz <Vanessaa_038@hotmail.com>
2026-03-18 01:38:18 -0500

    Updating utils.py with utility functions for timestamp formatting and pretty printing.

--- utils.py
+++ utils.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Utility functions for timestamp formatting and pretty printing
 """
 import json
 import time
-
 def now_ts():
     return time.strftime('%Y-%m-%d %H:%M:%S')
-
 def pretty(obj):
     try:
         print(json.dumps(obj, indent=2, ensure_ascii=False))
